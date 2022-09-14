fa07be1f83d3558a235f321436d0808d2b33293a
gdevincenzo <gdevincenzo@codearchitects.com>
2022-09-14 17:19:49 +0200

    Revert "fix: download-binary.ps1 interaction with requirements.json properties to solve "" error"
    This reverts commit 46e8065f562cc9cf704d88532b42bd97987c50f7.

--- scripts/download-binary.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [string]$link,
-    [string]$outPath
-)
-
-#Scarica dipendenze necessarie puntando alle Properties "DownloadLink" e "DownloadOutFile" del file requirements.json
-Invoke-RestMethod $link -OutFile $outPath
