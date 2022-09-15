40f8ecc80dd94f1557674e879daf65ef07fcdffa
gdevincenzo <gdevincenzo@codearchitects.com>
2022-09-15 14:46:48 +0200

    refactor: CheckRequirement json to administrator.ps1

--- scripts/requirements/proxy-npm.ps1
+++ scripts/requirements/proxy-npm.ps1
@@ -13,7 +13,7 @@ elseif ($ProxyDataSplit.Count -eq 3) {
 if ($InternetSettings.ProxyEnable -eq 0) {
     return @($true, 'OK')
 }
-elseif ((Test-NetConnection -ComputerName $ProxyAddress -Port $ProxyPort).TcpTestSucceeded) {
+elseif ( (Test-NetConnection -ComputerName $ProxyAddress -Port $ProxyPort).TcpTestSucceeded ) {
     return @($true, 'KO')
 }
 else {
--- scripts/requirements/virtual-machine.ps1
+++ scripts/requirements/virtual-machine.ps1
@@ -1,6 +1,6 @@
 $ComputerModel = (Get-CimInstance win32_computersystem).model
 
-if (($ComputerModel -ne 'VMware Virtual Platform') -and ($ComputerModel -ne 'Virtual Machine') -and ($ComputerModel -ne 'Macchina Virtuale')) {
+if ( ($ComputerModel -ne 'VMware Virtual Platform') -and ($ComputerModel -ne 'Virtual Machine') -and ($ComputerModel -ne 'Macchina Virtuale') ) {
     return @($true, 'OK')
 }
 else {
