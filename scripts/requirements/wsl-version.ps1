29c18cc2583c1984392ec0af9080633474ea7061
gdevincenzo <gdevincenzo@codearchitects.com>
2022-09-15 12:55:12 +0200

    refactor: CheckRequirements json to wsl-version.ps1

---
+++ scripts/requirements/wsl-version.ps1
@@ -0,0 +1,23 @@
+param(
+  [string]$scarConfig
+)
+
+if (!$scarConfig.Contains('terranova')) {
+    $wslStatus = (wsl --status)
+    $wslStatusSplit = ([System.Text.Encoding]::Unicode.GetString([System.Text.Encoding]::Default.GetBytes($wslStatus))).split(' ')
+    $wslVersionResult = $false
+    
+foreach ($item in $wslStatusSplit) {
+    if ($item -eq '2') {
+        $wslVersionResult = $true
+    }
+}
+if ($wslVersionResult) {
+    return @($true, 'OK')
+} else {
+    return @($true, 'KO')
+}
+}
+else {
+    return @($true, 'OK')
+}
