a32a874e7c56e25b7b0a1f34d0129bf3f78a76ab
fsilletti <fsilletti@codearchitects.com>
2022-09-15 11:07:38 +0200

    refactor: bring a sample requirement in ps

---
+++ scripts/requirements/proxy.ps1
@@ -0,0 +1,22 @@
+$ProxyData = Get-ItemProperty -Path 'Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings' | Select-Object 'Proxy*';
+
+if ($ProxyData.ProxyEnable -eq 1) {
+  $ProxyDataSplit = $ProxyData.ProxyServer -split ':'
+  if ($ProxyDataSplit.Count -eq 2) {
+    $ProxyAddress = $ProxyDataSplit[0]
+    $ProxyPort = $ProxyDataSplit[1]
+  }
+  else {
+    $ProxyAddress = $ProxyDataSplit[1].replace('/', '')
+    $ProxyPort = $ProxyDataSplit[2]
+  }
+  if ((Test-NetConnection -ComputerName $ProxyAddress -Port $ProxyPort).TcpTestSucceeded) {
+    return @($true, 'KO')
+  }
+  else {
+    return @($false, 'KO')
+  }
+}
+else {
+  return @($true, 'OK')
+}
